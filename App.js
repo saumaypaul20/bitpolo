3382be3461eb7d63c8f219ce29af3fca9c11e907
saumay <saumay@Sagars-MacBook-Pro.local>
2020-06-09 08:35:28 +0530

    wallet screen going on

--- App.js
+++ App.js
@@ -2,18 +2,27 @@ import React, { useEffect, useState } from 'react';
 import Signin from './src/pages/Signin/Signin';
 import Routes from './src/Routes/Routes';
 import Storage from './src/utils/storage.utils';
+import DeviceInfo  from 'react-native-device-info';
 
 const App= () => {
   const [login, setLogin] = useState(null)
 
-  useEffect(() => {
-    let user =Storage.get("login")
-     
+  const getUser = async ()=>{
+    let user = await Storage.get("login")
     if(user){
-        setLogin(true)
-    }else{
-      setLogin(false)
-    }
+      setLogin(true)
+  }else{
+    setLogin(false)
+  }
+  }
+
+  const deviceAccess =async()=>{
+    let deviceId = DeviceInfo.getDeviceId();
+    console.log(deviceId)
+  }
+  useEffect(() => {
+     getUser()
+     deviceAccess()
   }, [])
 
   return (
--- src/pages/Tabs/Wallet/Wallet.js
+++ src/pages/Tabs/Wallet/Wallet.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Image } from 'react-native'
 import { SafeAreaView } from 'react-native-safe-area-context'
+import Toolbar from '../../../components/Toolbar/Toolbar'
+import { Content, Button, Container } from 'native-base'
+import { Images, Colors, Fonts } from '../../../theme'
+import BPText from '../../../common/BPText/BPText'
 
 const Wallet = () => {
     return (
-        <SafeAreaView style={{flex:1,justifyContent:'center', alignItems:'center'}}>
-            <Text>Wallet</Text>
+        <SafeAreaView style={{flex:1}}>
+            <Container style={{ flex: 1,  backgroundColor: Colors.primeBG}}>
+                {/* <StatusBar translucent barStyle={Colors.barStyle}  backgroundColor="transparent" /> */}
+                <Content contentContainerStyle={{ flexGrow: 1 }}>
+                    <View style={{flex:1, justifyContent:'flex-start'}}>
+                        
+                        <BPText style={{marginHorizontal:12, fontSize:12, paddingVertical:16}}>
+                            Total Value (BTC) <BPText style={{fontFamily:Fonts.FONT_MEDIUM, fontSize:11,}}> 0 BTC</BPText> = $0.00
+                        </BPText>
+
+                        <View style={{
+                            backgroundColor:  Colors.darkGray3,
+                            padding:16
+                        }}>
+
+                            <BPText>Estimated value</BPText>
+ 
+                            <BPText style={{fontFamily: Fonts.FONT_MEDIUM, letterSpacing: 1.89, fontSize:24}}>0.00000000 BTC </BPText>
+                        </View>
+                        
+                    
+
+
+                    
+
+                    </View>
+                </Content>
+            </Container>
         </SafeAreaView>
     )
 }
